406fd2cd1e75207d24a27e0abbb6e0165643305f
AWS ParallelCluster user <ec2-user@ip-10-0-6-113.ap-southeast-1.compute.internal>
2024-09-02 04:16:06 +0000

    added pipeline-slurm.sh from lab 3

--- pipeline-slurm.sh
+++ pipeline-slurm.sh
@@ -2,15 +2,15 @@
 
 # Submit this script with: sbatch <this-filename>
 
-#SBATCH --time=1:00:00   # walltime
+#SBATCH --time=24:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 
-#SBATCH -J "example-job"   # job name
+#SBATCH -J "pipe"   # job name
 
 ## /SBATCH -p general # partition (queue)
-#SBATCH -o slurm.%N.%j.out # STDOUT
-#SBATCH -e slurm.%N.%j.err # STDERR
+#SBATCH -o pipe-slurm.%N.%j.out # STDOUT
+#SBATCH -e pipe-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
@@ -32,12 +32,5 @@ DPT.objects.processDirs(level='channel', cmd='import PyHipp as pyh; from PyHipp 
 print(time.localtime()); \
 print(time.time()-t0);"
 
-aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:xxxxxx:awsnotify --message "JobDone"
-
-#SBATCH --time=24:00:00   # walltime
-
-#SBATCH -J "pipe"   # job name
-
-#SBATCH -o pipe-slurm.%N.%j.out # STDOUT
-#SBATCH -e pipe-slurm.%N.%j.err # STDERR
+aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:180294188152:awsnotify --message "JobDone"
 
